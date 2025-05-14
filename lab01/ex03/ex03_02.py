b5422143ce5a00ac352f232b73dcd75d04d3e534
hanvi24 <vingochan24@gmail.com>
2025-05-14 11:25:01 +0700

    [update] lab01 ex03

---
+++ lab01/ex03/ex03_02.py
@@ -0,0 +1,6 @@
+def dao_nguoc_list(lst):
+    return lst[::-1]
+input_list = input("nhập danh sách các số, cách nhau bởi dấu phẩy:")
+numbers =  list(map(int, input_list.split(',')))
+list_dao_nguoc = dao_nguoc_list(numbers)
+print("List sau đảo ngược là: ", list_dao_nguoc)
